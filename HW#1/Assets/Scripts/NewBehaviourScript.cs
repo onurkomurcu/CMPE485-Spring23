5ee66c9e7ec1d2f32ca99f5e04f8ef69dc300b39
onurkmr <konur2727@gmail.com>
2023-03-11 18:06:54 +0300

    Applied force to the middle row leftmost column cube

--- HW#1/Assets/Scripts/NewBehaviourScript.cs
+++ HW#1/Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public Rigidbody body;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.AddForce(Vector3.left * 50);
     }
 }
